bd870f2de41d2c0124cb4b2b5d22cc6d377a8d08
sam walton <sam@foofrancine.com>
2012-10-23 10:29:54 -0500

    rails new . -T --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wublog::Application.config.secret_token = '1a40d3ea42e49a5be55119ec2f370e5c39d6b118b713ea66ce4224d6c3cee94fd6ddb3e27a3adab83f25f133d09026738a9a5c085816f602018b97a6090f661b'
